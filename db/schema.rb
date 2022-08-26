833ab48c5ee4ddb91d8d52c1b97c3910297e2869
Kei <kt10195665@yahoo.ne.jp>
2022-08-27 00:37:24 +0900

    Add: party(政党)モデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_26_153029) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_26_153434) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,6 +60,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_26_153029) do
 
   create_table "emblems", force: :cascade do |t|
     t.integer "limit_num", null: false
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "parties", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
