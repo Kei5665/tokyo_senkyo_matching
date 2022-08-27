2fbd7baf62bce98a38c7affcb77586f8713320f0
Kei <kt10195665@yahoo.ne.jp>
2022-08-27 18:52:40 +0900

    Fix: Mergeの修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,9 @@
 Rails.application.routes.draw do
   root 'staric_pages#top'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
   resources :questions, only: %i[show] do
     post 'answer', on: :member
   end
+  resources :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_27_052232) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_27_070409) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -61,6 +61,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_27_052232) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "party_questions", "parties"
