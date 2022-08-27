67b27f04802da1d8bc179fc44c818e944570462f
Kei <kt10195665@yahoo.ne.jp>
2022-08-27 19:30:13 +0900

    Add: ゲストログインのルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@
 
 Rails.application.routes.draw do
   root 'staric_pages#top'
+
+  get '/guest_login', to: 'user_sessions#guest_login'
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
   resources :questions, only: %i[show] do
     post 'answer', on: :member
   end
-  resources :users
 end
