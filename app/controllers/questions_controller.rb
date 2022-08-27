4d78d7a678af48dd45da348b0672f5d9f007a8fb
lei <mia.wang90@gmail.com>
2022-08-27 18:58:00 +0900

    Add: 次の質問へ遷移するメソッド追加

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,5 +4,41 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    # user = current_user
+    user = User.find(1)
+    @user_question = user.user_questions.find_by(question_id: params[:id])
+    result = params[:result]
+
+    # ユーザのresultを更新する
+    # result更新にエラーが出たので、後で対応
+    # case result
+    #   when "超賛成"
+    #     user_question.result.great!
+    #   when "賛成"
+    #     user_question.result.good!
+    #   when "反対"
+    #     user_question.result.bad!
+    # end
+
+    # 賛成/反対の意見を持つ政党リストを取得する
+
+
+    # user_partiesテーブルのpointを加算する
+    count_point(result)
+
+    # next_questionへ遷移する
+    next_page(@user_question)
+  end
+
+  def count_point(result)
+  end
+
+  def next_page(user_question)
+    if user_question.next?
+      @next_question = user_question.next
+      redirect_to question_path(@next_question)
+    else
+      redirect_to 結果ページ_path
+    end
   end
 end
--- app/models/user_question.rb
+++ app/models/user_question.rb
@@ -3,4 +3,14 @@ class UserQuestion < ApplicationRecord
   belongs_to :question
 
   enum :result, { great: 0, good: 1, bad: 2, pending: 3 }
+
+  def next
+    # user_questions = UserQuestion.where(user_id: current_user.id)
+    user_questions = UserQuestion.where(user_id: 1)
+    user_questions.where("question_id > ?", question_id).first
+  end
+
+  def next?
+    self.next.present?
+  end
 end
