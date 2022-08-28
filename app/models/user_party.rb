077ddee3916351fc9e7d085094fe4b37bad2618b
lei <mia.wang90@gmail.com>
2022-08-28 10:31:46 +0900

    Add: ポイント計算メソッド名変更;ランキングscope追加

--- app/models/user_party.rb
+++ app/models/user_party.rb
@@ -2,9 +2,9 @@ class UserParty < ApplicationRecord
   belongs_to :user
   belongs_to :party
 
+  scope :ranking, -> { order(point: :desc) }
+
   def calculate_point(user_question)
-    # user = current_user
-    user = User.find(1)
     # 政党意見を参照するため、party_questionsテーブルから政党を取得してopinionを参照する
     party_questions = PartyQuestion.where(question_id: user_question.question_id)
     party_question = party_questions.find_by(party_id: party_id)
@@ -12,11 +12,11 @@ class UserParty < ApplicationRecord
     user_result = user_question.result
 
     #　ユーザーと政党の意見が一致する場合、政党pointを加点する
-    if user_question.agree_with_party(party_question)
+    if user_question.agree_with_party?(party_question)
       add_point(user_result)
     # ユーザーと政党の意見が正反対の場合、政党pointを減点する
     # 政党が中立やユーザー意見が無回答の場合は、処理なし
-    elsif user_question.disagree_with_party(party_question)
+    elsif user_question.disagree_with_party?(party_question)
       reduce_point(user_result)
     end
   end
--- app/models/user_question.rb
+++ app/models/user_question.rb
@@ -38,32 +38,32 @@ class UserQuestion < ApplicationRecord
   end
 
   # ユーザ意見が政党と一致する場合
-  def agree_with_party(party_question)
-    both_agree(party_question) || both_disagree(party_question)
+  def agree_with_party?(party_question)
+    both_agree?(party_question) || both_disagree?(party_question)
   end
 
   # ユーザ意見が政党と不一致する場合
-  def disagree_with_party(party_question)
-    positive_disagree(party_question) || negative_disagree(party_question)
+  def disagree_with_party?(party_question)
+    user_positive_but_party_disagree?(party_question) || user_negative_but_party_agree?(party_question)
   end
 
   # 議題に対して、ユーザも政党も賛成する場合
-  def both_agree(party_question)
+  def both_agree?(party_question)
     result_positive? && party_question.agree?
   end
 
   # 議題に対して、ユーザも政党も反対する場合
-  def both_disagree(party_question)
+  def both_disagree?(party_question)
     result_negative? && party_question.disagree?
   end
 
   # 議題に対して、ユーザが賛成で、政党が反対する場合
-  def positive_disagree(party_question)
+  def user_positive_but_party_disagree?(party_question)
     result_positive? && party_question.disagree?
   end
 
   # 議題に対して、ユーザが反対で、政党が賛成する場合
-  def negative_disagree(party_question)
+  def user_negative_but_party_agree?(party_question)
     result_negative? && party_question.agree?
   end
 end
