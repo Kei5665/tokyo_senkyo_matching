d7d78dee0b83e9c1d4aface0ddcaed586d77730c
takeda_ryoya <t.ryooya@gmail.com>
2022-08-28 16:23:40 +0900

    Add: 棒グラフにポイントも渡すように設定

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,11 +6,15 @@ class StaticPagesController < ApplicationController
     @results = user_parties.ranking
 
     parties = []
+    points = []
 
     @results.each do |result|
+      point = result.point
       party_name = result.party.name
       parties.push(party_name)
+      points.push(point)
     end
     gon.parties = parties
+    gon.points = points
   end
 end
