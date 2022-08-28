0a70195156966adc9c69ebcd5a7c212b76fbc33b
takeda_ryoya <t.ryooya@gmail.com>
2022-08-28 15:30:09 +0900

    Add: 棒グラフの作成

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,5 +4,13 @@ class StaticPagesController < ApplicationController
   def result
     user_parties = UserParty.where(user_id: current_user.id)
     @results = user_parties.ranking
+
+    parties = []
+
+    @results.each do |result|
+      party_name = result.party.name
+      parties.push(party_name)
+    end
+    gon.parties = parties
   end
 end
