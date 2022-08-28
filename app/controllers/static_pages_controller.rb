29140fc1ad7c386b6599852435453dcfc1f36978
takeda_ryoya <t.ryooya@gmail.com>
2022-08-28 14:14:23 +0900

    Add: resultアクションを追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 class StaticPagesController < ApplicationController
   def top; end
 
-  def result; end
+  def result
+    user_parties = UserParty.where(user_id: current_user.id)
+    @results = user_parties.ranking
+  end
 end
