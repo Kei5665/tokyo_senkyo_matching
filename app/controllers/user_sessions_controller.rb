e8d413b9d5fa5e3dacea1a670b3fe93cd7c50cf7
Kei <kt10195665@yahoo.ne.jp>
2022-09-01 14:32:53 +0900

    Fix: 必要ない独自メソッドを削除、sorceryのauto_loginを追加

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -22,7 +22,7 @@ class UserSessionsController < ApplicationController
     random_value = SecureRandom.hex
     # email uniqueness validationが設定したので、ゲストログインは必須としない
     user = User.create!(name: random_value, role: 0)
-    login_as(user)
+    auto_login(user)
     # ユーザーと全政党の紐付け
     user.party_relation
 
@@ -31,11 +31,4 @@ class UserSessionsController < ApplicationController
 
     redirect_to question_path(first_question), notice: 'スタート！'
   end
-
-  private
-
-    def login_as(user)
-      session[:user_id] = user.id
-      @current_user = user
-    end
 end
