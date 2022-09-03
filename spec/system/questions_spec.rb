133d9573472207cc989e56e868e4f091eedab42d
Kei <kt10195665@yahoo.ne.jp>
2022-09-03 22:12:41 +0900

    Fix: click_onをboforeで切り出してitの前に行わせる

--- spec/system/questions_spec.rb
+++ spec/system/questions_spec.rb
@@ -21,8 +21,8 @@ RSpec.describe 'Questions', type: :system do
 
   describe '個別質問に対しての政党ポイント計算機能' do
     context 'ユーザが超賛成を押した場合' do
+      before {click_on '超賛成'}
       it '賛成意見を持つ政党のポイントが2点で、反対意見を持つ政党のポイントが-1点になる' do
-        click_on '超賛成'
         expect(page).to have_content '政党_1'
         expect(party_ranking_names).to eq(['政党_1', '政党_2'])
         expect(party_ranking_points).to eq([2, -1])
@@ -30,8 +30,8 @@ RSpec.describe 'Questions', type: :system do
     end
 
     context 'ユーザが賛成を押した場合' do
+      before {click_on '賛成'}
       it '賛成意見を持つ政党のポイントが1点で、反対意見を持つ政党のポイントが-1点になる' do
-        click_on '賛成'
         expect(page).to have_content '政党_1'
         expect(party_ranking_names).to eq(['政党_1', '政党_2'])
         expect(party_ranking_points).to eq([1, -1])
@@ -39,8 +39,8 @@ RSpec.describe 'Questions', type: :system do
     end
 
     context 'ユーザが反対を押した場合' do
+      before {click_on '反対'}
       it '賛成意見を持つ政党のポイントが-1点で、反対意見を持つ政党のポイントが1点になる' do
-        click_on '反対'
         expect(page).to have_content '政党_2'
         expect(party_ranking_names).to eq(['政党_2', '政党_1'])
         expect(party_ranking_points).to eq([1, -1])
@@ -48,8 +48,8 @@ RSpec.describe 'Questions', type: :system do
     end
 
     context 'ユーザが無回答を押した場合' do
+      before {click_on '無回答'}
       it 'どちらの政党ポイントも0点のまま' do
-        click_on '無回答'
         expect(page).to have_content '政党_1'
         expect(party_ranking_names).to eq(['政党_1', '政党_2'])
         expect(party_ranking_points).to eq([0, 0])
@@ -62,11 +62,12 @@ RSpec.describe 'Questions', type: :system do
       party1_agree_question2
       party2_disagree_question2
       click_on '無回答'
+      expect(page).to have_content '議題_2'
     end
 
     context '一つ目は無回答で、次の質問はユーザが超賛成を押した場合' do
+      before {click_on '超賛成'}
       it '賛成意見を持つ政党のポイント合計が2点で、反対意見を持つ政党のポイント合計が-1点になる' do
-        click_on '超賛成'
         expect(page).to have_content '政党_1'
         expect(party_ranking_names).to eq(['政党_1', '政党_2'])
         expect(party_ranking_points).to eq([2, -1])
@@ -74,8 +75,8 @@ RSpec.describe 'Questions', type: :system do
     end
 
     context '一つ目は無回答で、ユーザが賛成を押した場合' do
+      before {click_on '賛成'}
       it '賛成意見を持つ政党のポイントが1点で、反対意見を持つ政党のポイントが-1点になる' do
-        click_on '賛成'
         expect(page).to have_content '政党_1'
         expect(party_ranking_names).to eq(['政党_1', '政党_2'])
         expect(party_ranking_points).to eq([1, -1])
@@ -83,8 +84,8 @@ RSpec.describe 'Questions', type: :system do
     end
 
     context '一つ目は無回答で、次の質問はユーザが反対を押した場合' do
+      before {click_on '反対'}
       it '賛成意見を持つ政党のポイント合計が-1点で、反対意見を持つ政党のポイント合計が1点になる' do
-        click_on '反対'
         expect(page).to have_content '政党_2'
         expect(party_ranking_names).to eq(['政党_2', '政党_1'])
         expect(party_ranking_points).to eq([1, -1])
@@ -92,8 +93,8 @@ RSpec.describe 'Questions', type: :system do
     end
 
     context '一つ目は無回答で、次の質問はユーザが無回答を押した場合' do
+      before {click_on '無回答'}
       it 'どちらの政党ポイントも0点のまま' do
-        click_on '無回答'
         expect(page).to have_content '政党_1'
         expect(party_ranking_names).to eq(['政党_1', '政党_2'])
         expect(party_ranking_points).to eq([0, 0])
